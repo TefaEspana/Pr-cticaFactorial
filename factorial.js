47077dcf3cf2f78a2f33d95a7c2a1ce55f41242e
TefaEspaña <estefaniaespana18@gmail.com>
2025-05-31 21:23:58 -0600

    eje fac

---
+++ factorial.js
@@ -0,0 +1,8 @@
+const prompt = require ("prompt-sync")({sigint:true})
+let numero = parseInt(prompt("Ingrese un número: "));
+let factorial = 1;
+
+for (let i = 1; i<= numero; i++){
+    factorial*=i;
+}
+console.log(`El factorial de ${numero} es: ${factorial}`);
